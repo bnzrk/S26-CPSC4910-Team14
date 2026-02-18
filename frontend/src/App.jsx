5268a8bc6a0415f03fc94fcc9b686ceaa905562a
epatel4 <epatel2@g.clemson.edu>
2026-02-18 10:28:57 -0500

    updates to the app... adding a link to sign in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import AboutPage from './pages/About/AboutPage';
 import LoginPage from './pages/Login/LoginPage';
-
 import './App.scss';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <nav style={{ padding: '1rem', textAlign: 'right' }}>
+        <Link to="/login">Sign In</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<AboutPage />} />
         <Route path="/about" element={<AboutPage />} />
