4704321bd162a51f372730019f835c71b6ed18cd
Ben <127999784+bnzrk@users.noreply.github.com>
2026-02-26 00:42:07 -0500

    Use card component on point rules page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,18 +12,22 @@ import PointRulesPage from './pages/PointsRules/PointRulesPage';
 import PointsPage from './pages/Points/PointsPage'
 import './App.scss';
 
-export default function App() {
+export default function App()
+{
   const navigate = useNavigate();
 
   const { data: user, isLoading } = useCurrentUser();
 
   if (!isLoading)
-    console.log(`Current user: ${JSON.stringify(user)}`);  
+    console.log(`Current user: ${JSON.stringify(user)}`);
 
-  async function handleLogout() {
-    try {
+  async function handleLogout()
+  {
+    try
+    {
       await apiFetch("/auth/logout", { method: "POST" });
-    } catch (err) {
+    } catch (err)
+    {
       console.error("Logout failed:", err);
     }
 
@@ -55,12 +59,15 @@ export default function App() {
         <Route path="/" element={<AboutPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={
-  
           <GuestRoute>
             <LoginPage />
           </GuestRoute>
-          } />
-        <Route path="/point-rules" element={<PointRulesPage />} />
+        } />
+        <Route path="/point-rules" element={
+          <ProtectedRoute allowedUserTypes={[USER_TYPES.SPONSOR]}>
+            <PointRulesPage />
+          </ProtectedRoute>
+        } />
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/points" element={
           <ProtectedRoute allowedUserTypes={[USER_TYPES.DRIVER]}>
